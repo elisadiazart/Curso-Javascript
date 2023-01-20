6f69000a5bbb0a6ddedcf3584bc6f037bfae4e6f
Elisa <elisadiazartcontact@gmail.com>
2023-01-20 18:56:23 +0100

    20.01.23

---
+++ quiz-game /instrucciones.md
@@ -0,0 +1,20 @@
+## Cómo funciona
+
+- Habrá 4 temas sobre los que preguntar y cada tema tendrá 5 preguntas.
+
+  - Programación
+  - Matemáticas
+  - Ciencia
+  - Historia
+
+- Una vez que tengas el juego funcionando, añade un contador para que sólo te permita tener 5 segundos para contestar cada pregunta
+
+## Pasos a seguir
+
+- El primer paso es guardar en un array todas las preguntas, de esa forma será más facil filtrar y mostrar las que están contestadas o no.
+
+- Después deberás filtrar las que no han sido respondidas aún para seleccionar una aleatoria y mostrarla por pantalla.
+
+- Una vez que la tengas imprime por consola la pregunta, las 4 respuestas posibles y el tema al que pertenece
+
+- Ya puedes seguir tu alumn.gender==='male' ? solo : sola 😃
---
+++ quiz-game /js/scripts.js
@@ -0,0 +1,174 @@
+const gameQuestion = document.getElementById('game-question');
+const answersContainer = document.getElementById('answers');
+const startButton = document.getElementById('start-button');
+
+const QUESTIONS = {
+  programming: [
+    {
+      question: '¿Cuántos tipos de bucles hay en programación?',
+      answers: {
+        options: ['1', '2', '3', '50'],
+        correctAnswer: 1
+      },
+      theme: 'programming',
+      hasAnswered: false
+    },
+    {
+      question: '¿Cuántos lenguajes de programación entienden los navegadores?',
+      answers: {
+        options: ['3', '2', '1', '20'],
+        correctAnswer: 2
+      },
+      theme: 'programming',
+      hasAnswered: false
+    }
+  ],
+  math: [
+    {
+      question: '¿Cuánto es 5 x 8?',
+      answers: {
+        options: ['40', '25', '30', '50'],
+        correctAnswer: 0
+      },
+      theme: 'math',
+      hasAnswered: false
+    },
+    {
+      question: '¿Cuánto es la cuarta parte de la tercera parte?',
+      answers: {
+        options: ['Un doceavo', 'Un séptimo', '3/4 partes', '4/6 partes'],
+        correctAnswer: 0
+      },
+      theme: 'math',
+      hasAnswered: false
+    }
+  ],
+  science: [
+    {
+      question: '¿En qué dirección apunta la aguja de una brújula?',
+      answers: {
+        options: ['Sur', 'Este', 'Oeste', 'Norte'],
+        correctAnswer: 3
+      },
+      theme: 'science',
+      hasAnswered: false
+    },
+    {
+      question: '¿Qué mineral se encuentra en una solución salina?',
+      answers: {
+        options: ['Potasio', 'Calcio', 'Sodio', 'Magnesio'],
+        correctAnswer: 2
+      },
+      theme: 'science',
+      hasAnswered: false
+    },
+    {
+      question: '¿Cuántos huesos tiene un humano adulto?',
+      answers: {
+        options: ['412', '287', '306', '328'],
+        correctAnswer: 2
+      },
+      theme: 'science',
+      hasAnswered: false
+    }
+  ],
+  history: [
+    {
+      question: '¿En qué año piso el hombre la luna por primera vez?',
+      answers: {
+        options: ['1830', '1969', '2001', 'Fue un montaje'],
+        correctAnswer: 1
+      },
+      theme: 'history',
+      hasAnswered: false
+    },
+    {
+      question: '¿Dónde se inventó la pólvora?',
+      answers: {
+        options: ['China', 'Francia', 'EEUU', 'Italia'],
+        correctAnswer: 0
+      },
+      theme: 'history',
+      hasAnswered: false
+    },
+    {
+      question: '¿Dónde originaron los juegos olímpicos?',
+      answers: {
+        options: ['Cuenca', 'Grecia', 'Alemania', 'Dublin'],
+        correctAnswer: 1
+      },
+      theme: 'history',
+      hasAnswered: false
+    }
+  ]
+};
+
+const allQuestions = [
+  ...QUESTIONS.programming,
+  ...QUESTIONS.math,
+  ...QUESTIONS.science,
+  ...QUESTIONS.history
+];
+
+const score = {
+  programming: 0,
+  math: 0,
+  science: 0,
+  history: 0
+};
+
+let questionsFiltered;
+
+const questionsAnswered = () => {
+  return allQuestions.filter(question => !question.hasAnswered);
+};
+
+const randomNumber = () => {
+  return Math.floor(Math.random() * questionsFiltered.length);
+};
+
+let question;
+
+const printRandomQuestion = () => {
+  answersContainer.innerHTML = '';
+  questionsFiltered = questionsAnswered();
+  question = questionsFiltered[randomNumber()];
+  gameQuestion.textContent = question.question;
+  const fragment = document.createDocumentFragment();
+  for (let index = 0; index <= question.answers.options.length - 1; index++) {
+    const newAnswer = document.createElement('div');
+    newAnswer.classList.add('answer');
+    newAnswer.dataset.option = index;
+    newAnswer.textContent = question.answers.options[index];
+    fragment.append(newAnswer);
+  }
+  answersContainer.append(fragment);
+};
+
+const themePointsPrint = () => {
+  score[question.theme]++;
+  console.log(question.theme);
+  document.getElementById(`${question.theme}-score`).textContent =
+    score[question.theme];
+};
+
+startButton.addEventListener('click', () => {
+  startButton.style.display = 'none';
+  printRandomQuestion();
+});
+
+answersContainer.addEventListener('click', e => {
+  if (Number(e.target.dataset.option) === question.answers.correctAnswer) {
+    themePointsPrint();
+  }
+  question.hasAnswered = true;
+  printRandomQuestion();
+
+  if (questionsFiltered.length === 1) {
+    gameQuestion.textContent = 'TERMINASTE';
+    answersContainer.innerHTML = '';
+    const results = document.createElement('div');
+    results.textContent = `Has acertado 3 preguntas de ${allQuestions.length}`;
+    answersContainer.append(results);
+  }
+});
